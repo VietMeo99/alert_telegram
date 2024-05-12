858fd8cae67608704ef969ca657fa253fe2b402e
VuViet <vuhoangviet@cryptopie-labo.com>
2024-05-12 11:31:02 +0700

    clear code

---
+++ src/server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+import express from "express";
+import configViewEngine from "./config/viewEngine";
+import initWebRoutes from "./routes/web";
+import bodyParser from "body-parser";
+
+// "repository": {
+//    "type": "git",
+//    "url": "git+https://github.com/haryphamdev/telegram-bot-push-notification.git"
+// },
+
+let app = express();
+
+//config body-parser to post data to server
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//config view Engine
+configViewEngine(app);
+
+//init all web routes
+initWebRoutes(app);
+
+let port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`App is running at the port ${port}`);
+});
